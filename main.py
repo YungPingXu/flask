b2265a7de84fcff74418729442155b79d4a3f7da
YungPingXu <a0976373843@gmail.com>
2023-01-23 11:29:31 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 import os
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return 'citrus website'
 
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    print(request.args)
+    return request.args
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
