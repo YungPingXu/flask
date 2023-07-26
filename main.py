9e2a08a00d1487f224f4b31f4d3f34019e50c057
YungPingXu <a0976373843@gmail.com>
2023-07-26 15:44:19 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 import os
 
 app = Flask(__name__, static_folder='static')
@@ -11,7 +11,7 @@ def index():
 def send():
     args = request.args.to_dict()
     print(args)
-    return args
+    return redirect('https://discord.com')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
